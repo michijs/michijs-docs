a4357eac07557b87d876ab4501810534e409e6af
Lucas Segurado <lsegurado1996@gmail.com>
2024-11-07 10:38:35 +0100

    Added NotoSans and some bugfixes -moved to jekyll-src

--- michi.config.ts
+++ michi.config.ts
@@ -2,7 +2,7 @@ import type { ServerConfigFactory } from "@michijs/dev-server";
 
 export const config: ServerConfigFactory = () => ({
     esbuildOptions: {
-        outdir: "assets/js",
+        outdir: "jekyll-src/assets/js",
     }
 });
 
--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,23 @@ const ol = document.createElement("ol");
 aside.appendChild(ol);
 
 const li = document.createElement("li");
-ol.appendChild(li);
 const a = document.createElement("a");
 li.appendChild(a);
 
 document.querySelectorAll("[id]").forEach((x) => {
   if (["H2", "H3"].includes(x.tagName)) {
-    const linkClone = a.cloneNode(true) as HTMLLinkElement;
+    const liClone = li.cloneNode(true);
+    const linkClone = liClone.firstChild as HTMLLinkElement;
     linkClone.href = `#${x.id}`;
-    linkClone.textContent = x.textContent;
-    x.textContent = '';
-    x.append(linkClone)
-    if (x.tagName === "H3") a.textContent = x.textContent;
+    if (x.tagName === "H3") linkClone.textContent = x.textContent;
     else {
       const strong = document.createElement("strong");
-      a.append(strong);
+      linkClone.append(strong);
       strong.textContent = x.textContent;
     }
+    x.textContent = '';
+    x.append(linkClone.cloneNode(true))
+    ol.append(liClone)
   }
 });
 
